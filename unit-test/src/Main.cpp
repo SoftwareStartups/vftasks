9acfd16ee680326a50fe21c18fbe40558ccf9828
martijn@vectorfabrics.com <martijn@vectorfabrics.com>
2010-07-13 18:15:29 +0200

    add xml output

--- unit-test/src/Main.cpp
+++ unit-test/src/Main.cpp
@@ -6,13 +6,19 @@
 // agreement in place, no usage or distribution rights are granted by Vector
 // Fabrics.
 //
+#include <fstream>
 #include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/XmlOutputter.h>
 #include <cppunit/ui/text/TestRunner.h>
 
 int main()
 {
   CppUnit::Test *suite;                // test suite
-  CppUnit::TextUi::TestRunner runner;  // runner
+  CppUnit::TextUi::TestRunner runner;
+  std::ofstream outputFile("result.xml");
+
+  // Specify XML output and inform the test runner of this format (optional)
+  CppUnit::XmlOutputter xmlOutputter(&runner.result(), outputFile);
 
   // get the top-level suite from the registry
   suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
@@ -22,6 +28,8 @@ int main()
 
   // run the tests
   bool succeeded = runner.run();
+  xmlOutputter.write();
+  outputFile.close();
 
   // return 0 if all tests were succeeded, 1 if any test failed
   return succeeded ? 0 : 1;
