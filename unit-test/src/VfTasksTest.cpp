e60732f0dcd3d67a758b1414f2d3854e76faecd3
tommy@vectorfabrics.com <tommy@vectorfabrics.com>
2011-04-14 12:37:51 +0200

    VFA-1552: added some basic worker thread unit tests.

--- unit-test/src/VfTasksTest.cpp
+++ unit-test/src/VfTasksTest.cpp
@@ -10,7 +10,12 @@
 
 #include <cstdlib>  // for malloc, free
 
+#define X 32
+#define y 32
+#define N_PARTITIONS 4
+
 // a task that computes the square of an integer
+// can be used as a task without subsidiary workers
 static void *square(void *raw_args)
 {
   int *arg;     // a pointer to the argument
@@ -27,6 +32,153 @@ static void *square(void *raw_args)
 
   // return a pointer to the result
   return result;
+}
+
+void VfTasksTest::tearDown()
+{
+  if (this->pool != NULL)
+    vftasks_destroy_pool(pool);
+
+  if (this->args != NULL)
+    free(args);
+}
+
+void VfTasksTest::testCreateEmptyPool()
+{
+  vftasks_pool_t *pool = vftasks_create_pool(0);
+  CPPUNIT_ASSERT(pool == NULL);
+}
+
+void VfTasksTest::testCreateInvalidPool()
+{
+  vftasks_pool_t *pool = vftasks_create_pool(-1);
+  CPPUNIT_ASSERT(pool == NULL);
+}
+
+void VfTasksTest::testCreatePool1()
+{
+  vftasks_pool_t *pool = vftasks_create_pool(1);
+  CPPUNIT_ASSERT(pool != NULL);
+  vftasks_destroy_pool(pool);
+}
+
+void VfTasksTest::testCreatePool4()
+{
+  vftasks_pool_t *pool = vftasks_create_pool(4);
+  CPPUNIT_ASSERT(pool != NULL);
+}
+
+void VfTasksTest::testDestroyPool()
+{
+  vftasks_pool_t *pool = vftasks_create_pool(4);
+  vftasks_destroy_pool(pool);
+}
+
+void VfTasksTest::testSubmitEmptyTask()
+{
+  this->pool = vftasks_create_pool(1);
+  CPPUNIT_ASSERT(vftasks_submit(pool, NULL, NULL, 0) != 0);
+}
+
+void VfTasksTest::testSubmit()
+{
+  int arg = 3;
+
+  this->pool = vftasks_create_pool(1);
+  CPPUNIT_ASSERT(vftasks_submit(pool, square, &arg, 0) == 0);
+}
+
+void VfTasksTest::testSubmitInvalidNumWorkers()
+{
+  int arg = 3;
+
+  this->pool = vftasks_create_pool(1);
+
+  CPPUNIT_ASSERT(vftasks_submit(pool, square, &arg, -1) != 0);
+  CPPUNIT_ASSERT(vftasks_submit(pool, square, &arg, 1) != 0);
+}
+
+void VfTasksTest::testSubmitGet()
+{
+  int arg = 3;
+  int *result_ptr;
+
+  this->pool = vftasks_create_pool(1);
+
+  CPPUNIT_ASSERT(vftasks_submit(pool, square, &arg, 0) == 0);
+  CPPUNIT_ASSERT(vftasks_get(pool, (void **)&result_ptr) == 0);
+  CPPUNIT_ASSERT(*result_ptr == 9);
+}
+
+static void *loop(void *raw_args)
+{
+  args_t *args = (args_t *)raw_args;
+
+  int i;
+  int *acc = (int *)malloc(sizeof(int));
+  *acc = 0;
+
+  for (i = args->start; i < X; i += args->stride)
+    *acc += i;
+
+  return acc;
+}
+
+void VfTasksTest::submit_loop()
+{
+  int k;
+
+  this->args = (args_t *)calloc(N_PARTITIONS, sizeof(args_t));
+
+  for (k = 0; k < N_PARTITIONS; k++)
+  {
+    args[k].start = k;
+    args[k].stride = N_PARTITIONS;
+    CPPUNIT_ASSERT(vftasks_submit(this->pool, loop, &this->args[k], 0) == 0);
+  }
+}
+
+int VfTasksTest::get_loop()
+{
+  int k;
+  int acc = 0;
+  int *result;
+
+  for (k = 0; k < N_PARTITIONS; k++)
+  {
+
+    CPPUNIT_ASSERT(vftasks_get(pool, (void **)&result) == 0);
+    acc+= *result;
+  }
+
+  return acc;
+}
+
+void VfTasksTest::testSubmitLoop()
+{
+  this->pool = vftasks_create_pool(N_PARTITIONS);
+  this->submit_loop();
+}
+
+void VfTasksTest::testSubmitGetLoop()
+{
+  this->pool = vftasks_create_pool(N_PARTITIONS);
+  this->submit_loop();
+  CPPUNIT_ASSERT(get_loop() == 496);
+}
+
+void *nested_loop(void *raw_args)
+{
+  args_t *args = (args_t *)raw_args;
+
+  int i;
+  int *acc = (int *)malloc(sizeof(int));
+  *acc = 0;
+
+  for (i = args->start; i < X; i += args->stride)
+    *acc += i;
+
+  return acc;
 }
 
 // register fixture
--- unit-test/src/VfTasksTest.h
+++ unit-test/src/VfTasksTest.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 Vector Fabrics B.V. All rights reserved.
+// Copyright (c) 2011 Vector Fabrics B.V. All rights reserved.
 //
 // This file contains proprietary and confidential information of Vector
 // Fabrics and all use (including distribution) is subject to the conditions of
@@ -16,15 +16,53 @@ extern "C"
 #include <vftasks.h>
 }
 
+typedef struct
+{
+  int start;
+  int stride;
+} args_t;
+
 class VfTasksTest : public CppUnit::TestFixture
 {
   CPPUNIT_TEST_SUITE(VfTasksTest);
+
+  CPPUNIT_TEST(testCreateEmptyPool);
+  CPPUNIT_TEST(testCreateInvalidPool);
+  CPPUNIT_TEST(testCreatePool1);
+  CPPUNIT_TEST(testCreatePool4);
+  CPPUNIT_TEST(testDestroyPool);
+
+  CPPUNIT_TEST(testSubmitEmptyTask);
+  CPPUNIT_TEST(testSubmit);
+  CPPUNIT_TEST(testSubmitInvalidNumWorkers);
+  CPPUNIT_TEST(testSubmitGet);
+  CPPUNIT_TEST(testSubmitLoop);
+  CPPUNIT_TEST(testSubmitGetLoop);
+
   CPPUNIT_TEST_SUITE_END();  // VfTasksTest
 
 public:
+  void testCreateEmptyPool();
+  void testCreateInvalidPool();
+  void testCreatePool1();
+  void testCreatePool4();
+  void testDestroyPool();
+
+  void testSubmitEmptyTask();
+  void testSubmit();
+  void testSubmitInvalidNumWorkers();
+  void testSubmitGet();
+  void testSubmitLoop();
+  void testSubmitGetLoop();
+
+  void tearDown();
 
 private:
+  void submit_loop();
+  int get_loop();
+
   vftasks_pool_t *pool;  // pointer to a worker-thread pool
+  args_t *args;
 };
 
 #endif  // VFTASKSTEST_H
